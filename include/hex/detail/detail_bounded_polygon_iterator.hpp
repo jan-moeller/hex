20a67f29e6905df33ad67133e5ad6a4d93ed9a87
Jan Möller <drag-on@gmx.de>
2024-02-28 22:55:08 +0100

    Implement bounded_polygon_iterator

---
+++ include/hex/detail/detail_bounded_polygon_iterator.hpp
@@ -0,0 +1,114 @@
+//
+// MIT License
+//
+// Copyright (c) 2024 Jan Möller
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+#ifndef DETAIL_BOUNDED_POLYGON_ITERATOR_HPP
+#define DETAIL_BOUNDED_POLYGON_ITERATOR_HPP
+
+#include "hex/coordinate.hpp"
+#include "hex/vector.hpp"
+
+#include <iterator>
+
+#include <cstddef>
+
+namespace hex::detail
+{
+template<typename T>
+class bounded_polygon_iterator
+{
+  public:
+    using value_type        = vector<T>;
+    using reference         = value_type;
+    using difference_type   = std::ptrdiff_t;
+    using iterator_category = std::bidirectional_iterator_tag;
+
+    constexpr bounded_polygon_iterator() = default;
+    constexpr bounded_polygon_iterator(r_coordinate<T> rmin,
+                                       s_coordinate<T> smin,
+                                       r_coordinate<T> rmax,
+                                       s_coordinate<T> smax,
+                                       vector<T>       v)
+        : m_rmin(rmin)
+        , m_smin(smin)
+        , m_rmax(rmax)
+        , m_smax(smax)
+        , m_v(v)
+    {
+    }
+
+    constexpr auto operator++() noexcept -> bounded_polygon_iterator&
+    {
+        using namespace literals;
+        if (m_v.r() < m_rmax && m_v.s() > m_smin)
+            m_v.set(m_v.q(), m_v.r() + 1_r);
+        else
+        {
+            m_v.set(m_v.q() + 1_q, m_rmin);
+            if (m_v.s() > m_smax)
+                m_v.set(m_v.q(), m_smax);
+        }
+        return *this;
+    }
+    constexpr auto operator++(int) noexcept -> bounded_polygon_iterator
+    {
+        auto cp = *this;
+        ++(*this);
+        return cp;
+    }
+
+    constexpr auto operator--() noexcept -> bounded_polygon_iterator&
+    {
+        using namespace literals;
+        if (m_v.r() > m_rmin && m_v.s() < m_smax)
+            m_v.set(m_v.q(), m_v.r() - 1_r);
+        else
+        {
+            m_v.set(m_v.q() - 1_q, m_rmax);
+            if (m_v.s() < m_smin)
+                m_v.set(m_v.q(), m_smin);
+        }
+        return *this;
+    }
+    constexpr auto operator--(int) noexcept -> bounded_polygon_iterator
+    {
+        auto cp = *this;
+        --(*this);
+        return cp;
+    }
+
+    constexpr auto operator*() const noexcept -> vector<T> { return m_v; }
+
+    constexpr auto operator==(bounded_polygon_iterator const&) const -> bool = default;
+    constexpr auto operator<=>(bounded_polygon_iterator const&) const        = default;
+
+  private:
+    r_coordinate<T> m_rmin;
+    s_coordinate<T> m_smin;
+    r_coordinate<T> m_rmax;
+    s_coordinate<T> m_smax;
+    vector<T>       m_v;
+};
+} // namespace hex::detail
+
+#endif // DETAIL_BOUNDED_POLYGON_ITERATOR_HPP
---
+++ test/detail/test_bounded_polygon_iterator.cpp
@@ -0,0 +1,89 @@
+//
+// MIT License
+//
+// Copyright (c) 2024 Jan Möller
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+#include "hex/detail/detail_bounded_polygon_iterator.hpp"
+
+#include <catch2/catch_all.hpp>
+
+#include <iterator>
+
+using namespace hex;
+using namespace hex::detail;
+
+TEST_CASE("bounded_polygon_iterator", "[detail]")
+{
+    using namespace hex::literals;
+
+    SECTION("concept checks")
+    {
+        STATIC_CHECK(std::weakly_incrementable<bounded_polygon_iterator<int>>);
+        STATIC_CHECK(std::input_or_output_iterator<bounded_polygon_iterator<int>>);
+        STATIC_CHECK(std::indirectly_readable<bounded_polygon_iterator<int>>);
+        STATIC_CHECK(std::input_iterator<bounded_polygon_iterator<int>>);
+        STATIC_CHECK(std::forward_iterator<bounded_polygon_iterator<int>>);
+        STATIC_CHECK(std::bidirectional_iterator<bounded_polygon_iterator<int>>);
+    }
+
+    SECTION("default constructed must compare equal")
+    {
+        STATIC_CHECK(bounded_polygon_iterator<int>() == bounded_polygon_iterator<int>());
+    }
+    SECTION("++iter")
+    {
+        bounded_polygon_iterator<int> iter(-1_r, -1_s, 2_r, 1_s, vector{-2_q, 1_r});
+        ++iter;
+        CHECK(*iter == vector{-2_q, 2_r});
+        ++iter;
+        CHECK(*iter == vector{-1_q, 0_r});
+    }
+    SECTION("iter++")
+    {
+        bounded_polygon_iterator<int> iter(-1_r, -1_s, 2_r, 1_s, vector{-2_q, 1_r});
+        CHECK(*(iter++) == vector{-2_q, 1_r});
+        CHECK(*iter == vector{-2_q, 2_r});
+    }
+    SECTION("--iter")
+    {
+        bounded_polygon_iterator<int> iter(-1_r, -1_s, 2_r, 1_s, vector{-1_q, 1_r});
+        --iter;
+        CHECK(*iter == vector{-1_q, 0_r});
+        --iter;
+        CHECK(*iter == vector{-2_q, 2_r});
+    }
+    SECTION("iter--")
+    {
+        bounded_polygon_iterator<int> iter(-1_r, -1_s, 2_r, 1_s, vector{-1_q, 1_r});
+        CHECK(*(iter--) == vector{-1_q, 1_r});
+        CHECK(*iter == vector{-1_q, 0_r});
+    }
+
+    SECTION("ordering")
+    {
+        constexpr bounded_polygon_iterator<int> a(-1_r, -1_s, 2_r, 1_s, vector{-2_q, 1_r});
+        constexpr bounded_polygon_iterator<int> b(-1_r, -1_s, 2_r, 1_s, vector{-1_q, 1_r});
+        constexpr bounded_polygon_iterator<int> c(-1_r, -1_s, 2_r, 1_s, vector{-1_q, 2_r});
+        STATIC_CHECK(a < b);
+        STATIC_CHECK(b < c);
+        STATIC_CHECK(b == b);
+    }
+}
