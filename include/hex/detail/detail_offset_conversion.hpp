26173e5fbf85de95fd5253e2485ed17694e3ea12
Jan Möller <drag-on@gmx.de>
2024-03-15 23:34:43 +0100

    Implement cubic->offset conversion functions

--- include/hex/detail/detail_offset_conversion.hpp
+++ include/hex/detail/detail_offset_conversion.hpp
@@ -45,11 +45,25 @@ constexpr auto from_odd_q(T x, T y) noexcept -> vector<T>
     return vector<T>(q, r);
 }
 template<typename T>
+constexpr auto to_odd_q(vector<T> v) noexcept -> std::array<T, 2>
+{
+    auto const y = v.q().value();
+    auto const x = v.r().value() + (y - (y & 1)) / 2;
+    return {x, y};
+}
+template<typename T>
 constexpr auto from_even_q(T x, T y) noexcept -> vector<T>
 {
     auto const q = q_coordinate<T>{y};
     auto const r = r_coordinate<T>{x - (y + (y & 1)) / 2};
     return vector<T>(q, r);
+}
+template<typename T>
+constexpr auto to_even_q(vector<T> v) noexcept -> std::array<T, 2>
+{
+    auto const y = v.q().value();
+    auto const x = v.r().value() + (y + (y & 1)) / 2;
+    return {x, y};
 }
 template<typename T>
 constexpr auto from_odd_r(T x, T y) noexcept -> vector<T>
@@ -59,11 +73,25 @@ constexpr auto from_odd_r(T x, T y) noexcept -> vector<T>
     return vector<T>(r, s);
 }
 template<typename T>
+constexpr auto to_odd_r(vector<T> v) noexcept -> std::array<T, 2>
+{
+    auto const y = v.r().value();
+    auto const x = v.s().value() + (y - (y & 1)) / 2;
+    return {x, y};
+}
+template<typename T>
 constexpr auto from_even_r(T x, T y) noexcept -> vector<T>
 {
     auto const r = r_coordinate<T>{y};
     auto const s = s_coordinate<T>{x + (-(y & 1) - y) / 2};
     return vector<T>(r, s);
+}
+template<typename T>
+constexpr auto to_even_r(vector<T> v) noexcept -> std::array<T, 2>
+{
+    auto const y = v.r().value();
+    auto const x = v.s().value() + (y + (y & 1)) / 2;
+    return {x, y};
 }
 template<typename T>
 constexpr auto from_odd_s(T x, T y) noexcept -> vector<T>
@@ -73,11 +101,25 @@ constexpr auto from_odd_s(T x, T y) noexcept -> vector<T>
     return vector<T>(q, s);
 }
 template<typename T>
+constexpr auto to_odd_s(vector<T> v) noexcept -> std::array<T, 2>
+{
+    auto const y = v.s().value();
+    auto const x = v.q().value() + (y - (y & 1)) / 2;
+    return {x, y};
+}
+template<typename T>
 constexpr auto from_even_s(T x, T y) noexcept -> vector<T>
 {
     auto const q = q_coordinate<T>{x - (y + (y & 1)) / 2};
     auto const s = s_coordinate<T>{y};
     return vector<T>(q, s);
+}
+template<typename T>
+constexpr auto to_even_s(vector<T> v) noexcept -> std::array<T, 2>
+{
+    auto const y = v.s().value();
+    auto const x = v.q().value() + (y + (y & 1)) / 2;
+    return {x, y};
 }
 template<typename T>
 constexpr auto select_offset_to_cubic_function(coordinate_axis axis, offset_parity parity) -> vector<T> (*)(T, T)
@@ -86,6 +128,17 @@ constexpr auto select_offset_to_cubic_function(coordinate_axis axis, offset_pari
         {&from_even_q, &from_odd_q},
         {&from_even_r, &from_odd_r},
         {&from_even_s, &from_odd_s},
+    }};
+    return funs[std::to_underlying(axis)][std::to_underlying(parity)];
+}
+template<typename T>
+constexpr auto select_cubic_to_offset_function(coordinate_axis axis,
+                                               offset_parity   parity) -> std::array<T, 2> (*)(vector<T>)
+{
+    static constexpr std::array<std::array<std::array<T, 2> (*)(vector<T>), 2>, 3> funs{{
+        {&to_even_q, &to_odd_q},
+        {&to_even_r, &to_odd_r},
+        {&to_even_s, &to_odd_s},
     }};
     return funs[std::to_underlying(axis)][std::to_underlying(parity)];
 }
